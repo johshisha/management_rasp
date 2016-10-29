01ca6c9c065677343245ca64bf40e05e7cc98043
johshisha <johshisha@gmail.com>
2016-10-29 21:11:25 +0900

    [WIP] development fluentd environment

---
+++ mydns/update_mydns-example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+user=user
+pass=pass
+wget --http-user=${user} --http-passwd=${pass} http://www.mydns.jp/login.html
--- run_app.sh
+++ run_app.sh
@@ -1,1 +1,5 @@
-
+for app in 'voice_recipe'
+do
+cd /home/sato/${app}
+nohup /home/sato/.pyenv/shims/uwsgi --ini ${app}.ini &
+done
